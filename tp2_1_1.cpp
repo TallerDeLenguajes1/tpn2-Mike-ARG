42f0788a296aa89053c616c497207e875c08983c
Miguel <milito_vanetta@hotmail.com>
2020-04-07 04:01:27 -0300

    Primer commit

---
+++ tp2_1_1.cpp
@@ -0,0 +1,15 @@
+#define N 4
+#define M 5
+
+int main() {
+
+    int f,c;
+    double mt[N][M];
+    
+    for (f = 0; f < N; f++) {
+        for (f = 0; f < N; f++) {
+            cprintf("%lf ", mt[f][c];)
+        }
+        cprintf("\n");
+    }
+}
