143cd19a2f1d5e8b4dc22ed6579b06ec1edc162b
Miguel <milito_vanetta@hotmail.com>
2020-04-09 01:04:43 -0300

    Punto 4 corregido y terminado

--- Punto4.cpp
+++ Punto4.cpp
@@ -13,39 +13,120 @@ typedef struct compu compu;
 
 char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
-#define MAX 50
+#define MAX 100
 
-compu CargarPC(compu X);
-void mostrarPCs(compu X, int Y);
+compu cargarPC(compu X);
+void cargarArreglo(compu *X, int N);
+void mostrarPCs(compu *X, int N);
+void PCvieja(compu *X, int N);
+void PCveloz(compu *X, int N);
+
 
 
 int main() {
     int N;
-    compu compu1[N];
-
     srand(time(0));
 
     printf("Ingrese la cantidad de PCs a ingresar: ");
     scanf("%d", &N);
     fflush(stdin);
+    printf("\n\n");
+
+    compu compu1[N];
+    compu *arreglo;
+    arreglo = compu1;
+
+
+    cargarArreglo(arreglo, N);
+    mostrarPCs(arreglo, N);
+    PCvieja(arreglo, N);
+    PCveloz(arreglo, N);
+
+    getchar();
+    return 0;
 
 
 }
 
 compu cargarPC(compu X) {
-    X.velocidad = rand() % 3 + 1;
+    X.velocidad = (rand() % 3 + 1);
     X.anio = rand() % 8 + 2000;
     X.cantidad = rand() % 4 + 1;
-    X.tipo_cpu = t
+    X.tipo_cpu = tipos[rand() % 6];
+
+    return X;
 }
 
-void mostrarPCs(compu X, int Y) {
-    for (int i = 0; i < Y; i++) {
-        printf("PC Nro \n\n");
-        printf("Velocidad del procesador: %.2f\n", X[i].velocidad);
-        printf("Cantidad de núcleos: %d\n", X[i].cantidad);
-        printf("Tipo de CPU: %s\n", X[i].tipo_cpu);
-        printf("Año de fabricación: %d\n\n", X[i].anio);
+void cargarArreglo(compu *X, int N) {
+    compu aux;
+
+    for (int i = 0; i < N; i++) {
+        aux = cargarPC(aux);
+        *X = aux;
+        X++;
     }
 }
 
+void mostrarPCs(compu *X, int N) {
+    compu aux = *X;
+
+    for (int i = 0; i < N; i++) {
+        printf("PC Nro %d\n\n", i + 1);
+        printf("Velocidad del procesador: %.1f\n", aux.velocidad);
+        printf("Cantidad de núcleos: %d\n", aux.cantidad);
+        printf("Tipo de CPU: %s\n", aux.tipo_cpu);
+        printf("Año de fabricación: %d\n\n", aux.anio);
+        
+        X++;
+        aux = *X;
+    }
+}
+
+void PCvieja(compu *X, int N) {
+    compu aux = *X;
+    
+    compu PCmasVieja = aux;
+    int anioMenor = aux.anio;
+    int IDpcVieja = 1;
+    X++;
+
+    for (int i = 1; i < N; i++) {
+        if (aux.anio < anioMenor) {
+            anioMenor = aux.anio;
+            PCmasVieja = aux;
+            IDpcVieja = i + 1;
+        }
+        X++;
+        aux = *X;
+    }
+    printf("\n\nLa PC más vieja es la PC Nro %d:\n\n", IDpcVieja);
+    printf("Velocidad del procesador: %.1f\n", PCmasVieja.velocidad);
+    printf("Cantidad de núcleos: %d\n", PCmasVieja.cantidad);
+    printf("Tipo de CPU: %s\n", PCmasVieja.tipo_cpu);
+    printf("Año de fabricación: %d\n\n", PCmasVieja.anio);
+}
+
+void PCveloz(compu *X, int N) {
+    compu aux = *X;
+    
+    compu PCmasVeloz = aux;
+    int velMayor = aux.velocidad;
+    int IDpcVeloz = 1;
+    X++;
+
+    for (int i = 1; i < N; i++) {
+        if (aux.velocidad > velMayor) {
+            velMayor = aux.velocidad;
+            PCmasVeloz = aux;
+            IDpcVeloz = i + 1;
+        }
+        X++;
+        aux = *X;
+    }
+    printf("\n\nLa PC más veloz es la PC Nro %d:\n\n", IDpcVeloz);
+    printf("Velocidad del procesador: %.1f\n", PCmasVeloz.velocidad);
+    printf("Cantidad de núcleos: %d\n", PCmasVeloz.cantidad);
+    printf("Tipo de CPU: %s\n", PCmasVeloz.tipo_cpu);
+    printf("Año de fabricación: %d\n\n", PCmasVeloz.anio);
+
+}
